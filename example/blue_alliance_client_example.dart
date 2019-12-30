da1e7df860b6f90f5a8fc1e29aab70fcda9c56f3
Kai Page <kaibug@gmail.com>
2019-12-30 13:19:58 -0600

    Init commit

---
+++ example/blue_alliance_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:blue_alliance_client/blue_alliance_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/blue_alliance_client.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library blue_alliance_client;
+
+export 'src/blue_alliance_client_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
